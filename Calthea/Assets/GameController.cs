0b0ef46d4d2737ce90a4790ea9ed124133e14406
Charwyn Jewel G. Carpo <chgo.carpo.up@phinmaed.com>
2024-09-27 03:09:58 +0800

    checkpoint system

---
+++ Calthea/Assets/GameController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Vector2 checkpointPos;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        checkpointPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            Die();
+        }
+    }
+
+    public void UpdateCheckpoint(Vector2 pos)
+    {
+        checkpointPos = pos;
+    }
+
+    void Die()
+    {
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        rb.simulated = false;
+        rb.velocity = Vector2.zero;
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(duration);
+        transform.position = checkpointPos;
+        spriteRenderer.enabled = true;
+        rb.simulated = true;
+    }
+}
